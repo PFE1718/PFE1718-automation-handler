6d9d9762e7486265a52b01f3bd1afe3bbe299b67
Gauthier Leonard <gauthier_leonard@msn.com>
2018-01-24 16:49:00 +0100

    Updating requirements

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,10 @@
-#This file is for any packages you need installed for your skill to run
-sudo apt-get install the-best-packages ever
+# Shell script to run before being able to use the automation handler
+if [ ! -d "/opt/mycroft/habits" ]; then
+    sudo mkdir /opt/mycroft/habits
+    sudo chown -R $USER:$USER /opt/mycroft/habits
+    touch /opt/mycroft/habits/logs.json
+    touch /opt/mycroft/habits/habits.json
+    echo '[]' >> /opt/mycroft/habits/habits.json
+    touch /opt/mycroft/habits/triggers.json
+    echo '[]' >> /opt/mycroft/habits/triggers.json
+fi
