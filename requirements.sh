1f8b944a9a174c7e33ae248c658e26618a9bd517
Gauthier Leonard <gauthier_leonard@msn.com>
2018-01-24 17:28:00 +0100

    Update requirements.sh to allow editing of the files

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,10 @@
-# Shell script to run before being able to use the automation handler
+# Shell script to run before being able to use the skill
 if [ ! -d "/opt/mycroft/habits" ]; then
     sudo mkdir /opt/mycroft/habits
-    sudo chown -R $USER:$USER /opt/mycroft/habits
     touch /opt/mycroft/habits/logs.json
     touch /opt/mycroft/habits/habits.json
     echo '[]' >> /opt/mycroft/habits/habits.json
     touch /opt/mycroft/habits/triggers.json
     echo '[]' >> /opt/mycroft/habits/triggers.json
+    sudo chmod -R ugo+rw /opt/mycroft/habits
 fi
