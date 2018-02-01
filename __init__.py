9330fc71a60556dc8c8cb50522d62bf92bb5d72e
Gauthier Leonard <gauthier_leonard@msn.com>
2018-02-01 23:41:03 +0100

    Update skills checking

--- __init__.py
+++ __init__.py
@@ -542,7 +542,7 @@ class AutomationHandlerSkill(MycroftSkill):
 
         if not ret:
             self.set_context("InstallMissingContext")
-            dial = "To use this skill, you also have to install the skill"
+            dial = "To use the skill automation handler, you also have to install the skill"
             num_skill = "this skill"
             skills_list = ""
             for skill in self.to_install[:-1]:
